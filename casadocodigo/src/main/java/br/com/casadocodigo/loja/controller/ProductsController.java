8682d5bb43611908aaeefd85787b6f15029468ed
CTS\599158 <murilo.felpeto@cognizant.com>
2017-06-21 16:45:32 -0300

    Criado e meio configurado o JPA

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/ProductsController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/ProductsController.java
@@ -1,14 +1,22 @@
 package br.com.casadocodigo.loja.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.casadocodigo.loja.daos.ProductDAO;
+import br.com.casadocodigo.loja.models.Product;
 
 @Controller
 public class ProductsController {
 
+	@Autowired
+	private ProductDAO productDAO;
+	
 	@RequestMapping("/produtos")
-	public String save()
+	public String save(Product product)
 	{
+		productDAO.save(product);
 		System.out.println("Cadastrando o produto");
 		return "products/ok";
 	}
