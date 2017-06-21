608cc28ef1747ed50354cb5a6d3938b6ffe9d39a
CTS\599158 <murilo.felpeto@cognizant.com>
2017-06-21 14:35:06 -0300

    Criando o form de cadastro

---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/ProductsController.java
@@ -0,0 +1,21 @@
+package br.com.casadocodigo.loja.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductsController {
+
+	@RequestMapping("/produtos")
+	public String save()
+	{
+		System.out.println("Cadastrando o produto");
+		return "products/ok";
+	}
+	
+	@RequestMapping("/produtos/form")
+	public String form()
+	{
+		return "products/form";
+	}
+}
