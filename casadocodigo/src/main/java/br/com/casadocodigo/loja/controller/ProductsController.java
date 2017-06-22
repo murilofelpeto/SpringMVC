73e6d1b2c33569fc44b5e54a2514862b66363237
muril <murilofelpeto@gmail.com>
2017-06-21 22:24:29 -0300

    Hibernate 100% funcionando

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/ProductsController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/ProductsController.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.loja.controller;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import br.com.casadocodigo.loja.daos.ProductDAO;
 import br.com.casadocodigo.loja.models.Product;
 
 @Controller
+@Transactional
 public class ProductsController {
 
 	@Autowired
